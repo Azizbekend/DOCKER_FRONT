23adefd230aedcce452130a51e5a9264948de3d3
Azizbekend <azizma3105@gmail.com>
2025-12-04 16:15:53 +0300

    asdasdsads

--- server-save-image/index.js
+++ server-save-image/index.js
@@ -12,7 +12,7 @@ app.use(cors({
 // Настраиваем хранение
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "./public/hardware");
+        cb(null, __dirname + "public/hardware");
     },
     filename: function (req, file, cb) {
         const ext = path.extname(file.originalname);
