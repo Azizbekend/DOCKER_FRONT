e602ee04ac9e3ee1e3e461424a789c8069f492c9
Azizbekend <azizma3105@gmail.com>
2025-12-10 14:48:39 +0300

    asdasd

--- full-hydrix/src/modules/dispatcher/pages/tim-model/index.tsx
+++ full-hydrix/src/modules/dispatcher/pages/tim-model/index.tsx
@@ -3,7 +3,7 @@ export const TimModel = () => {
         <>
             <div className="informations-dispatch__timModel timModel-dispatch dispatch-background">
                 <div className="timModel-dispatch__container">
-                    <iframe width="1024" height="640" src="http://localhost:8668/player/player.html?load=../applications/Waterclean/Waterclean.gltf" className="timModel-dispatch__iframe"></iframe>
+                    <iframe width="1024" height="640" src="http://hydrig.gsurso.ru/tim-model/applications/Waterclean/Waterclean.html" className="timModel-dispatch__iframe"></iframe>
                 </div>
             </div>
         </>
