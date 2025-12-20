15be7b95a28c331c33f00c22094fe873fa464b96
Azizbekend <azizma3105@gmail.com>
2025-12-20 07:38:56 +0300

    asdasd

--- full-hydrix/src/modules/domain/pages/registry-objects/pages/service/api.tsx
+++
@@ -1,6 +0,0 @@
-import { PassportObject } from "@/app/api/api-router"
-import { reserchInstance } from "@/app/api/instances"
-
-export const passportObject = () => {
-    return reserchInstance.get(PassportObject.all)
-}
