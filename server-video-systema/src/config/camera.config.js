0c56fed364b8cc5313529b337e9e10f79783963f
Azizbekend <azizma3105@gmail.com>
2026-02-02 16:08:59 +0300

    asd

--- server-video-systema/src/config/camera.config.js
+++ server-video-systema/src/config/camera.config.js
@@ -1,10 +1,10 @@
 module.exports = [
-    { id: 1, rtsp: 'rtsp://admin:Shapshi@16@192.168.1.100:554/cam/realmonitor?channel=1&subtype=0' },
+    { id: 1, rtsp: 'rtsp://admin:Shapshi@16@192.168.1.100:554/cam/realmonitor?channel=7&subtype=0' },
     { id: 2, rtsp: 'rtsp://admin:Shapshi@16@192.168.1.100:554/cam/realmonitor?channel=6&subtype=0' },
     { id: 3, rtsp: 'rtsp://admin:Shapshi@16@192.168.1.100:554/cam/realmonitor?channel=3&subtype=0' },
     { id: 4, rtsp: 'rtsp://admin:Shapshi@16@192.168.1.100:554/cam/realmonitor?channel=4&subtype=0' },
     { id: 5, rtsp: 'rtsp://admin:Shapshi@16@192.168.1.100:554/cam/realmonitor?channel=5&subtype=0' },
     { id: 6, rtsp: 'rtsp://admin:Shapshi@16@192.168.1.100:554/cam/realmonitor?channel=2&subtype=0' },
-    { id: 7, rtsp: 'rtsp://admin:Shapshi@16@192.168.1.100:554/cam/realmonitor?channel=7&subtype=0' },
+    { id: 7, rtsp: 'rtsp://admin:Shapshi@16@192.168.1.100:554/cam/realmonitor?channel=1&subtype=0' },
 ];
 
