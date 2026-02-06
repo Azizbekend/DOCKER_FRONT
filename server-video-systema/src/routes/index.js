d3974fde8f556caca0fd57486b3d8c1b553f739f
Azizbekend <azizma3105@gmail.com>
2026-02-06 10:19:05 +0300

    фыв

--- server-video-systema/src/routes/index.js
+++ server-video-systema/src/routes/index.js
@@ -3,6 +3,20 @@ const cameraRoutes = require('./camera-routes');
 
 const router = Router();
 
+router.use((req, res, next) => {
+    if (req.url.endsWith('.m3u8')) {
+        // Chrome workaround
+        delete req.headers.range;
+
+        res.setHeader('Accept-Ranges', 'none');
+        res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+        res.setHeader('Pragma', 'no-cache');
+        res.setHeader('Expires', '0');
+        res.setHeader('Content-Type', 'application/vnd.apple.mpegurl');
+    }
+    next();
+});
+
 router.use('/cameras', cameraRoutes);
 
 module.exports = router;
