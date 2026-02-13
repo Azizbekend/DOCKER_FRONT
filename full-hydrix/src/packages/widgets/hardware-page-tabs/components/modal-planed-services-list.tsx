ded46108623d7944ec59f3bc43f43bff7f37e6c0
Azizbekend <azizma3105@gmail.com>
2026-02-13 18:14:41 +0300

    фыв

--- full-hydrix/src/packages/widgets/hardware-page-tabs/components/modal-planed-services-list.tsx
+++ full-hydrix/src/packages/widgets/hardware-page-tabs/components/modal-planed-services-list.tsx
@@ -38,7 +38,7 @@ export const ModalPlanedServicesList = observer(({ show, setShow, idPlaned }: Pr
                     {isLoaded && <Loader />}
                     {model.length > 0 ? (model.map((item) => (<RequestCard key={item.id} request={item} />))) : (
                         <div className="text-center py-12">
-                            <h3 className="text-lg font-medium text-gray-800 mb-1">Заявки не найдены</h3>
+                            <h3 className="text-lg font-medium text-gray-800 mb-1">История пустая</h3>
                             <p className="text-gray-600">Нет заявок</p>
                         </div>
                     )}
